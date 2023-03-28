eb3ce3e0fef9419f5038f037ca97aab79be231e9
Oleksandr Sherberh <skipfromua@gmail.com>
2023-03-28 13:14:44 +0300

    Fix standards typos

--- lib/stupidedi/transaction_sets/004010/standards/LB823.rb
+++ lib/stupidedi/transaction_sets/004010/standards/LB823.rb
@@ -14,23 +14,23 @@ module Stupidedi
             s::N1.use(30, r::Mandatory, d::RepeatCount.bounded(1)),
             s::DEP.use(40, r::Mandatory, d::RepeatCount.bounded(1)), # To indicate the lockbox ID, date, time, deposit number and bank account information
             s::AMT.use(50, r::Mandatory, d::RepeatCount.bounded(1)), # Total Amount
-            s::QTY.use(60, r::Mandatory, d::RepeatCount.bounded(2)), # Number of Batches AND Number of Checks/Trans
+            s::QTY.use(60, r::Mandatory, d::RepeatCount.bounded(2))), # Number of Batches AND Number of Checks/Trans
 
           d::TableDef.detail("2 - Detail",
             d::LoopDef.build("BAT", d::RepeatCount.bounded(100),
-              s::BAT.use( 10, r::Mandatory, d::RepeatCount.bounded(1))),
+              s::BAT.use( 10, r::Mandatory, d::RepeatCount.bounded(1)),
               s::AMT.use( 20, r::Mandatory, d::RepeatCount.bounded(1))), # Total Amount of Batch
 
             d::LoopDef.build("BPR", d::RepeatCount.bounded(100),
-              s::BPR.use( 30, r::Mandatory, d::RepeatCount.bounded(1))), # Beginning segment for Payment Order/Remittance Advice
-              s::REF.use( 40, r::Mandatory, d::RepeatCount.bounded(1))),
-              s::REF.use( 50, r::Mandatory, d::RepeatCount.bounded(1))), # Required by DMSi just eol
-              s::DTM.use( 60, r::Mandatory, d::RepeatCount.bounded(1))), # Date the Remittance was created
+              s::BPR.use( 30, r::Mandatory, d::RepeatCount.bounded(1)), # Beginning segment for Payment Order/Remittance Advice
+              s::REF.use( 40, r::Mandatory, d::RepeatCount.bounded(1)),
+              s::REF.use( 50, r::Mandatory, d::RepeatCount.bounded(1)), # Required by DMSi just eol
+              s::DTM.use( 60, r::Mandatory, d::RepeatCount.bounded(1)), # Date the Remittance was created
               s::N1.use( 70, r::Mandatory, d::RepeatCount.bounded(1))), # Customer Name
 
             d::LoopDef.build("RMR", d::RepeatCount.bounded(100),
-              s::RMR.use( 80, r::Mandatory, d::RepeatCount.bounded(1))), # Accounts receivable item(s)
-              s::REF.use( 90, r::Mandatory, d::RepeatCount.bounded(1))), # eol
+              s::RMR.use( 80, r::Mandatory, d::RepeatCount.bounded(1)), # Accounts receivable item(s)
+              s::REF.use( 90, r::Mandatory, d::RepeatCount.bounded(1)))), # eol
 
           d::TableDef.summary("3 - Summary",
             s:: SE.use( 10, r::Mandatory, d::RepeatCount.bounded(1))))
